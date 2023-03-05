b519d1559956c3ef022c3002e82d22386e916965
elhajuojy <souirimehdi311@gmail.com>
2023-03-05 11:23:47 +0100

    add and configure InertiaJs

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <h1>
+            Welcome to your first Inertia app!
+        </h1>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
