73d88630266e4475a77eb7230e3e4d7bbcf5572e
elhajuojy <souirimehdi311@gmail.com>
2023-03-05 16:51:53 +0100

    get use with inertia js and do some tricks

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,5 +1,7 @@
 <script setup >
-import {Link} from "@inertiajs/vue3"
+
+import TheNavVue from "../Shared/TheNav.vue"
+
 defineProps({
     name: {
         type: String,
@@ -15,25 +17,10 @@ defineProps({
 
 <template>
     <div class="container grid place-items-center divide-red-50  shadow-orange-50  m-auto text-center">
+        <TheNavVue />
         <h1 class="" >
             Home
         </h1>
-        <nav>
-            <ul class="flex flex-row justify-center">
-                <li class="mx-2">
-                    <Link href="/" class="hover:text-purple-100">Home</Link>
-                </li>
-                <li class="mx-2">
-                    <Link href="/users" class="hover:text-purple-100">Users</Link>
-                </li>
-
-                <li class="mx-2">
-                    <Link href="/settings" class="hover:text-purple-100">Settings</Link>
-                </li>
-
-            </ul>
-        </nav>
-
     </div>
 </template>
 
---
+++ resources/js/Shared/TheNav.vue
@@ -0,0 +1,28 @@
+<script setup>
+import {Link} from "@inertiajs/vue3"
+</script>
+
+<template>
+    <div>
+        <nav>
+            <ul class="flex flex-row justify-center">
+                <li class="mx-2">
+                    <Link href="/" class="hover:text-purple-100">Home</Link>
+                </li>
+                <li class="mx-2">
+                    <Link href="/users" class="hover:text-purple-100">Users</Link>
+                </li>
+
+                <li class="mx-2">
+                    <Link href="/settings" class="hover:text-purple-100">Settings</Link>
+                </li>
+
+            </ul>
+        </nav>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
