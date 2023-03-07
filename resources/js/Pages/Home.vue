5f6939b5c7e3744be6ac3df131c28b830af37b9c
elhajuojy <souirimehdi311@gmail.com>
2023-03-07 20:49:52 +0100

    add shoelace.

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,6 +1,8 @@
 <script setup >
 
 import TheNavVue from "../Shared/TheNav.vue"
+import '@shoelace-style/shoelace/dist/components/qr-code/qr-code.js';
+import '@shoelace-style/shoelace/dist/components/input/input.js';
 
 defineProps({
     name: {
@@ -20,7 +22,11 @@ defineProps({
         <h1 class="" >
             Home
         </h1>
+
         <TheNavVue />
+        <sl-input maxlength="255" clearable label="Value" v-model="qrCode"></sl-input>
+
+        <sl-qr-code :value="qrCode"></sl-qr-code>
     </div>
 </template>
 
