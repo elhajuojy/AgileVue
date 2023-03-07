a54b6fb7e570e3c87a11b71ec8c67e288deff611
elhajuojy <souirimehdi311@gmail.com>
2023-03-07 17:04:30 +0100

    Multiple Layout

---
+++ resources/js/Pages/Auth/Login.vue
@@ -0,0 +1,32 @@
+<script>
+
+import LayoutGuest from '../../Shared/LayoutGuest.vue';
+
+export default {
+    //   layout : null
+  layout: LayoutGuest,
+}
+
+</script>
+
+<script setup>
+
+
+
+</script>
+
+<template>
+    <Head>
+        <title> Login</title>
+    </Head>
+    <div class="max-w-3xl m-auto mt-6">
+        <div>
+            <h1 class="font-bold text-3xl">Login</h1>
+        </div>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
---
+++ resources/js/Shared/LayoutGuest.vue
@@ -0,0 +1,25 @@
+
+<script setup>
+import { Head } from '@inertiajs/vue3';
+
+
+
+</script>
+
+<template>
+    <Head>
+        <title>Guest  Layout</title>
+    </Head>
+    <div>
+        <header class="bg-gray-100 p-6 flex justify-between">
+            <h1 class="font-bold text-2xl">Guest  Layout</h1>
+            <TheNav/>
+        </header>
+        <slot />
+
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
