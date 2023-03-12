655e64fdf3e5f7bb730f7dcd1f56d9dfacc505f9
elhajuojy <souirimehdi311@gmail.com>
2023-03-12 12:08:21 +0100

    add vuetify to the project successfully

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,17 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 import '@shoelace-style/shoelace/dist/themes/light.css';
 import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path';
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 setBasePath('https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.3.0/dist/');
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Agile Vue';
 
@@ -16,11 +26,12 @@ createInertiaApp({
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
+            .use(vuetify)
             .use(plugin)
             .use(ZiggyVue, Ziggy)
             .mount(el);
     },
     progress: {
-        color: '#4B5563',
+        color: '#5299fd',
     },
 });
