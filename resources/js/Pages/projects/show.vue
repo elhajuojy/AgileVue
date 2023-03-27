7a498b191d06d7695095dce9061605dd7f51f51c
elhajuojy <souirimehdi311@gmail.com>
2023-03-27 12:31:01 +0000

    update size

--- resources/js/Pages/projects/show.vue
+++ resources/js/Pages/projects/show.vue
@@ -26,7 +26,7 @@ onMounted(() => {
         <AuthenticatedLayout>
 
             <section class="text-black flex m-auto">
-                <aside class="min-w-[25%] lg:min-w-[20%]  p-6 shadow h-screen bg-white">
+                <aside class="min-w-[20%] lg:min-w-[15%]  p-6 shadow h-screen bg-white">
                     <header class="flex gap-3 m-auto">
                         <img :src="props.project.project_cover" class="w-8 m-auto my-2 h-8 rounded-full">
                         <div class="flex-grow-1">
@@ -36,9 +36,21 @@ onMounted(() => {
                     </header>
                     <section class="mt-10">
                         <h3 class="text-xl font-bold mb-2">Planing</h3>
-                        <ul>
+                        <ul class="grid gap-3">
                             <li>
                                 <a href="" class="text-gray-500 hover:text-gray-600 mt-12 py-6">Board</a>
+                                <font-awesome-icon icon="fa-solid fa-game-board" />
+                            </li>
+                            <li>
+                                <a href="" class="text-gray-500 hover:text-gray-600 mt-12 py-6">Backlog</a>
+                                <font-awesome-icon icon="fa-solid fa-game-board" />
+                            </li>
+                            <li>
+                                <a href="" class="text-gray-500 hover:text-gray-600 mt-12 py-6">Board</a>
+                                <font-awesome-icon icon="fa-solid fa-game-board" />
+                            </li>
+                            <li>
+                                <a href="" class="text-gray-500 hover:text-gray-600 mt-12 py-6">Backlog</a>
                                 <font-awesome-icon icon="fa-solid fa-game-board" />
                             </li>
                         </ul>
