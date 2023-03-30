542db871ea99e7a861c119828972813be7a1b1d6
elhajuojy <souirimehdi311@gmail.com>
2023-03-30 20:54:50 +0000

    seed more users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,7 +27,7 @@ class UserFactory extends Factory
             'Job_description' => fake()->text(),
             'Job_location' => fake()->city(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => bcrypt("password"), // password
             'remember_token' => Str::random(10),
         ];
     }
