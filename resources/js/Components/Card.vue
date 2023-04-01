f957f53b353a594506150e68670f6725a799645a
elhajuojy <souirimehdi311@gmail.com>
2023-04-01 18:22:02 +0000

    create invitation card sample

---
+++ resources/js/Components/Card.vue
@@ -0,0 +1,66 @@
+<script setup>
+
+</script>
+
+<script>
+export default {
+    data: () => ({
+        show: false,
+    }),
+}
+</script>
+
+<template>
+
+
+    <v-card
+        width="400"
+        variant="outlined"
+    >
+        <template v-slot:title>
+            This is a title
+        </template>
+
+        <template v-slot:subtitle>
+            This is a subtitle
+        </template>
+
+        <template v-slot:text>
+           <div class="flex align-center  justify-space-between">
+            <p>
+                This is content
+            </p>
+            <div class="grid gap-2">
+                <v-btn
+                    class=" text-sm"
+                    rounded=true
+                    density="comfortable"
+
+                    @click="show = !show"
+                    variant="outlined"
+                >
+                    Accept
+                </v-btn>
+                <v-btn
+                    class=" text-sm"
+                    rounded=true
+                    density="comfortable"
+
+                    @click="show = !show"
+                    variant="outlined"
+                >
+                    Decline
+                </v-btn>
+            </div>
+           </div>
+
+        </template>
+
+    </v-card>
+</template>
+
+
+
+<style scoped>
+
+</style>
