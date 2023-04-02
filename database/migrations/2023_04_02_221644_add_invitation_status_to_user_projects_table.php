77160682ca26b0ccdd7b08410c0dfdf4f8bef0da
elhajuojy <souirimehdi311@gmail.com>
2023-04-02 22:21:00 +0000

    add invitation status to user_projects table

---
+++ database/migrations/2023_04_02_221644_add_invitation_status_to_user_projects_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('user_projects', function (Blueprint $table) {
+            //
+            $table->enum("invitation_status",["pending","accepted","rejected"])->default("pending");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('user_projects', function (Blueprint $table) {
+            //
+            $table->dropColumn("invitation_status");
+        });
+    }
+};
