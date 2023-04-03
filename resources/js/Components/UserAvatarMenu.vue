28e1199e0ceebf3b8ea973591a0ee5ab8be21894
elhajuojy <souirimehdi311@gmail.com>
2023-04-03 21:23:22 +0000

    Add User Avatar With Menu

---
+++ resources/js/Components/UserAvatarMenu.vue
@@ -0,0 +1,83 @@
+<script setup>
+
+
+const data = defineProps({
+    user : {
+        type : Object,
+        required : true
+    },
+})
+
+console.log(data.user)
+</script>
+
+<template>
+    <div>
+        <v-container
+            fluid
+            style="height: auto"
+
+        >
+            <v-row justify="center">
+                <v-menu
+                    min-width="23px"
+                    rounded
+                >
+                    <template v-slot:activator="{ props }">
+                        <v-btn
+                            icon
+                            v-bind="props"
+                            variant="plain"
+                            width="30"
+                            height="30"
+
+                        >
+                            <v-avatar
+
+                                size="30"
+                                :image="data.user.profile_cover"
+
+                            >
+                            </v-avatar>
+                        </v-btn>
+                    </template>
+                    <v-card>
+                        <v-card-text>
+                            <div class="mx-auto text-center">
+                                <v-avatar
+                                    color="brown"
+                                >
+                                    <span class="text-h5">mehdi </span>
+                                </v-avatar>
+                                <h3>mehdi </h3>
+                                <p class="text-caption mt-1">
+                                    mehdi@gmail.com
+                                </p>
+                                <v-divider class="my-3"></v-divider>
+                                <v-btn
+                                    rounded
+                                    variant="text"
+                                >
+                                    Edit Account
+                                </v-btn>
+                                <v-divider class="my-3"></v-divider>
+                                <v-btn
+                                    rounded
+                                    variant="text"
+                                >
+                                    Disconnect
+                                </v-btn>
+                            </div>
+                        </v-card-text>
+                    </v-card>
+                </v-menu>
+            </v-row>
+        </v-container>
+    </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+
+</style>
